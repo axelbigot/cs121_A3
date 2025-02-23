cd45a1ae6db93a08923fce1b9d4060be289ddccf
Axel Bigot <abigot@uci.edu>
2025-02-22 16:30:01 -0800

    fix: low memory tests

--- index_tests.py
+++ index_tests.py
@@ -1,11 +1,11 @@
 import unittest
 from dataclasses import fields
-from mailcap import subst
 from pathlib import Path
 
 import psutil
 
-from inverted_index import construct_index_recursively, Posting, _set_memory_low_th, InvertedIndex
+from inverted_index import construct_index_recursively, Posting, _set_memory_low_th, InvertedIndex, \
+    _FLUSH_MEMORY_THRESHOLD
 
 
 def print_trunc(o, chars: int = 500):
@@ -48,20 +48,25 @@ class IndexTests(unittest.TestCase):
     def test_flush_index_when_low_memory(self):
         # Always flush to disk.
         threshold = 1
-        _set_memory_low_th(threshold)
+        old_threshold = _FLUSH_MEMORY_THRESHOLD
 
-        subset = 'developer/DEV/alderis_ics_uci_edu'
-        index = InvertedIndex()
+        try:
+            _set_memory_low_th(threshold)
 
-        for page in Path(subset).rglob('*.json'):
-            mem = psutil.virtual_memory()
-            print(f'Available memory: {mem.available} / {mem.total} ({mem.percent}%)')
+            subset = 'developer/DEV/alderis_ics_uci_edu'
+            index = InvertedIndex()
 
-            if mem.percent < threshold * 100:
-                self.assertTrue(not index._internal)
-                print(f'Index flushed to disk ({threshold * 100}% threshold)')
+            for page in Path(subset).rglob('*.json'):
+                mem = psutil.virtual_memory()
+                print(f'Available memory: {mem.available} / {mem.total} ({mem.percent}%)')
 
-            index.add(page)
+                if mem.percent < threshold * 100:
+                    self.assertTrue(not index._internal)
+                    print(f'Index flushed to disk ({threshold * 100}% threshold)')
+
+                index.add(page)
+        finally:
+            _set_memory_low_th(old_threshold)
 
 if __name__ == '__main__':
     unittest.main()
