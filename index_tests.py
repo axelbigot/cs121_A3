3710b1f50955558ca8fb99be3b10a3c52656f15d
Axel Bigot <abigot@uci.edu>
2025-02-23 19:02:30 -0800

    feat: flush when memory is low

---
+++ index_tests.py
@@ -0,0 +1,67 @@
+import unittest
+from dataclasses import fields
+from mailcap import subst
+from pathlib import Path
+
+import psutil
+
+from inverted_index import construct_index_recursively, Posting, _set_memory_low_th, InvertedIndex
+
+
+def print_trunc(o, chars: int = 500):
+    print(f'{str(o)[:chars]}\n\n... (truncated)')
+
+class IndexTests(unittest.TestCase):
+    def test_recursive_constructor(self):
+        # Test on a subset of DEV.
+        subset = 'developer/DEV/alderis_ics_uci_edu'
+
+        index = construct_index_recursively(subset)
+        print_trunc(index)
+
+        # No token should have more postings than the number of pages.
+        page_count = sum((1 for _ in Path(subset).rglob('*.json')))
+        for token in index:
+            self.assertLessEqual(len(index[token]), page_count)
+
+    def test_write_index_to_disk(self):
+        subset = 'developer/DEV/alderis_ics_uci_edu'
+
+        index = construct_index_recursively(subset)
+        index.flush()
+
+        # Check that the disk was created.
+        self.assertTrue(index.disk_dir.exists())
+        # Check that the in-memory index was cleared
+        self.assertTrue(not index._internal)
+
+        for disk in index.disks:
+            with open(disk, 'r') as f:
+                content = f.read()
+                print_trunc(content)
+
+                # Check that the file contents are of posting form
+                # (there are better ways to do this)
+                for attr in fields(Posting):
+                    self.assertIn(f'"{attr.name}":', content)
+
+    def test_flush_index_when_low_memory(self):
+        # Always flush to disk.
+        threshold = 1
+        _set_memory_low_th(threshold)
+
+        subset = 'developer/DEV/alderis_ics_uci_edu'
+        index = InvertedIndex()
+
+        for page in Path(subset).rglob('*.json'):
+            mem = psutil.virtual_memory()
+            print(f'Available memory: {mem.available} / {mem.total} ({mem.percent}%)')
+
+            if mem.percent < threshold * 100:
+                self.assertTrue(not index._internal)
+                print(f'Index flushed to disk ({threshold * 100}% threshold)')
+
+            index.add(page)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ inverted_index.py
@@ -0,0 +1,161 @@
+import json
+import shutil
+from collections import defaultdict
+from dataclasses import dataclass
+from pathlib import Path
+from types import MappingProxyType
+from typing import Generator
+
+import platformdirs
+import psutil
+
+from JSONtokenizer import compute_word_frequencies, tokenize_JSON_file
+
+
+@dataclass
+class Posting:
+    """
+    Container for an inverted index document posting.
+    """
+    doc_id: int
+    frequency: int
+
+# The name of the entire A3 application.
+_APP_NAME = 'CS121_A3'
+# Local data dir for this application.
+_APP_DATA_DIR = Path(platformdirs.user_data_dir(_APP_NAME))
+_INDEXES_DIR = _APP_DATA_DIR / 'indexes'
+
+# Percentage of remaining memory at which time the index should be flushed to disk.
+_FLUSH_MEMORY_THRESHOLD = 0.5
+
+# Wipe the old indexes, if any.
+if _INDEXES_DIR.exists():
+    shutil.rmtree(_INDEXES_DIR)
+
+def _memory_low() -> bool:
+    """
+    Check if memory is too low.
+    Returns:
+        True if memory is too low, False otherwise.
+    """
+    # RAM & other virtual mem.
+    vmem = psutil.virtual_memory()
+    return vmem.available < vmem.total * _FLUSH_MEMORY_THRESHOLD
+
+def _set_memory_low_th(perc: float):
+    """
+    Set the global low memory threshold.
+
+    Args:
+        perc: Percentage of total memory that is considered low memory.
+    """
+    if perc > 1 or perc < 0:
+        raise ValueError('Percentage must be between 0 and 1.')
+
+    global _FLUSH_MEMORY_THRESHOLD
+    _FLUSH_MEMORY_THRESHOLD = perc
+
+class InvertedIndex:
+    """
+    An inverted index storing document Posting objects by token.
+    """
+    def __init__(self):
+        self._internal: dict[str, list[Posting]] = defaultdict(list)
+        self._disk_dir: Path = _INDEXES_DIR / f'index-{self.__hash__()}'
+
+    @property
+    def disk_dir(self) -> Path:
+        """
+        The directory containing this index's partitioned disk files.
+
+        Returns:
+            Path to disk directory.
+        """
+        return self._disk_dir
+
+    @property
+    def disks(self) -> Generator[Path, None, None]:
+        """
+        The index's partitioned disk files. This is where index content is stored, which may be
+        spread over multiple files.
+
+        Returns:
+            Generator of Paths pointing to disk files.
+        """
+        yield from self._disk_dir.iterdir()
+
+    def add(self, json_path: Path):
+        """
+        Assimilate a document into the inverted index.
+
+        Args:
+            json_path: Path to JSON document.
+        """
+        for token, freq in compute_word_frequencies(tokenize_JSON_file(json_path)).items():
+            # TODO: replace with real id once available.
+            self._internal[token].append(Posting(doc_id = 0, frequency = freq))
+
+        if _memory_low():
+            self.flush()
+
+    def flush(self):
+        """
+        Write the inverted index to disk and wipe its in-memory data. The location of the data
+        is somewhere in the user data i.e. C:/../AppData/Local/... in windows. This path is
+        accessible via the #disk_dir property.
+        """
+        path = self._disk_dir
+
+        path.mkdir(parents = True, exist_ok = True)
+        with open(path / 'disk.json', "w") as f:
+            json.dump(self._internal, f, default = lambda o: o.__dict__)
+
+        self._internal = defaultdict(list)
+
+    def __getitem__(self, token: str) -> list[Posting]:
+        """
+        Retrieve all Postings for a given token.
+
+        Args:
+            token: The token whose Postings to retrieve.
+
+        Returns:
+            A list of all Postings for a given token.
+        """
+        return self._internal[token]
+
+    def __iter__(self):
+        """
+        Iterate over all token-postings.
+
+        Returns:
+            Immutable iterable of token-postings dict.
+        """
+        return iter(MappingProxyType(self._internal))
+
+    def __str__(self) -> str:
+        """
+        String representation of the index.
+
+        Returns:
+            A string representation of the index.
+        """
+        return json.dumps(self._internal, default = lambda o: o.__dict__, indent = 2)
+
+def construct_index_recursively(root_dir_path: str) -> InvertedIndex:
+    """
+    Builds an inverted index from all json files in a directory, recursively adding all json
+    documents to the index.
+
+    Args:
+        root_dir_path: String path to the root directory.
+
+    Returns:
+        An in-memory inverted index containing all json documents in the root directory.
+    """
+    index = InvertedIndex()
+    for file in Path(root_dir_path).rglob('*.json'):
+        index.add(file)
+
+    return index
