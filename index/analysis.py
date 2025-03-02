8054464b02f070192c04e12521aa16a4b37f60ae
Axel Bigot <abigot@uci.edu>
2025-03-02 01:38:57 -0800

    feat: optimized inverted index

--- index/analysis.py
+++ index/analysis.py
@@ -1,9 +1,9 @@
 import os
 
 def generate_analysis(InvertedIndex) -> None:
-    print(f"Analysis of InvertedIndex with id: {InvertedIndex.id}")
+    print(f"Analysis of InvertedIndex with name: {InvertedIndex.name}")
     print("-" * 40)
-    print(f"# of indexed documents: {InvertedIndex.doc_count}")
+    print(f"# of indexed documents: {InvertedIndex.page_count}")
     print(f"# of unique words: {sum(1 for _ in InvertedIndex)}")
-    print(f"Total size of index on disk: {sum(os.path.getsize(path) for path in InvertedIndex.disk_partitions) / 1024} KB")   
+    print(f"Total size of index on disk: {sum(os.path.getsize(path) for path in InvertedIndex.disks) / 1024} KB")
 
--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+import argparse
+import logging
+
+import pyfiglet
+
 from index import InvertedIndex, generate_analysis
 
 
+parser = argparse.ArgumentParser(description = 'Main entry point for the A3 Search Engine')
+parser.add_argument('-d', '--debug', action = 'store_true', help = 'Enable debug mode')
+args = parser.parse_args()
+
+logging.basicConfig(level = logging.DEBUG if args.debug else logging.INFO,
+                    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
+
 def main():
-    index = InvertedIndex('developer')
+    print(pyfiglet.figlet_format('CS121 A3 G98', font = 'slant'))
+    logger.debug('Started Application in DEBUG mode')
+
+    index = InvertedIndex(
+        'developer',
+        name = 'index_main',
+        persist = True
+    )
     generate_analysis(index)
 
 if __name__ == '__main__':
