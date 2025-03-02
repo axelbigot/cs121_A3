d28ac798e950d2b7c1fa9e72b37808f5e9885f01
javiles204 <jasontaviles@gmail.com>
2025-03-02 11:54:49 -0800

    implement naive relevent document retrieval (#20)
    * implement searcher class; path_mapper can get url->docID and docID->url
    
    * moved searcher.py in folder
    
    ---------
    
    Co-authored-by: Jason Aviles <jtaviles@uci.edu>

--- retrieval/searcher.py
+++ retrieval/searcher.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-from index import InvertedIndex
-
+from index.inverted_index import InvertedIndex, Posting
+from index.path_mapper import PathMapper
 
 class Searcher:
     """
@@ -13,6 +13,7 @@ class Searcher:
     def __init__(self, source_dir_path: str | Path):
         # The inverted index providing access to the data source.
         self._index = InvertedIndex(source_dir_path)
+        self.path_mapper = PathMapper(source_dir_path)
 
     def search(self, query: str) -> list[str]:
         """
@@ -24,5 +25,40 @@ class Searcher:
         Returns:
             List of page urls ordered by relevance.
         """
-        # TODO: Implement (#17)
-        return list(f'https://www.example{count}.com' for count in range(100))
+        
+        query_tokens = query.lower().split()
+        doc_scores = {}
+        
+        for token in query_tokens:
+            token_docs = self._index[token]
+
+            for posting in token_docs:
+                doc_id = posting.doc_id
+                count = posting.frequency
+
+                if doc_id in doc_scores:
+                    doc_scores[doc_id] += count
+                else:
+                    doc_scores[doc_id] = count
+        
+        # filter documents to only include all query tokens
+        filtered_docs = {
+            doc : score 
+            for doc, score in doc_scores.items()
+            if all(any(p.doc_id == doc for p in self._index[token])
+            for token in query_tokens)
+        }
+
+        # sort documents by relevance score
+        sorted_docs = sorted(filtered_docs.items(), key = lambda x : x[1], reverse = True)
+
+        # get the urls based on document id
+        result_urls = [
+            self.path_mapper.get_url_by_id(doc_id) 
+            for doc_id, _ in sorted_docs 
+            if self.path_mapper.get_url_by_id(doc_id)
+        ]
+        
+        return result_urls
+
+
